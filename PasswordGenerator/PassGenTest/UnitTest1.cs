9d1e68234974f40b80a933715c05d966a29ead83
lukaspanni <lukas-panni@web.de>
2018-05-17 20:37:37 +0200

    Uploaded Project

---
+++ PasswordGenerator/PassGenTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordGeneratorFunctions;
+using System;
+
+namespace PassGenTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCorrectLength(){
+            string[] res = new string[10];
+            int[] len = { 5, 8, 6, 3, 7, 8, 5, 1, 2, 3 };
+            for (int i = 0; i < 10; i++) {
+                res[i] = PasswordGeneratorFunctions.PasswordGeneratorFunctions.generatePassword(len[i]);
+                if(Assert.Equals(res[i], len[i])) {
+                    Console.WriteLine("true");
+                }
+                else {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestNull() {
+            int[] len = { 0, -1, -8, -9 };
+            for(int i = 0; i < 4; i++) {
+                Assert.IsNull(PasswordGeneratorFunctions.PasswordGeneratorFunctions.generatePassword(len[i]), "failed");
+            }
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordGeneratorFunctions/PasswordGeneratorFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PasswordGeneratorFunctions
+{
+    public static class PasswordGeneratorFunctions
+    {
+        public static string generatePassword(int length=8) {
+            if (length < 1)
+                return null;
+            char[] passArr = new char[length];
+            Random rnd = new Random();
+            for (int i = 0; i < length; i++) {
+                passArr[i] = (char)rnd.Next(33, 126);
+            }
+            return new string(passArr);
+        }
+
+    }
+}
