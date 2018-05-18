a3cf48a96c454429a223a6c752975b6a9e28e728
Lukas Panni <lukas.panni@outlook.de>
2018-05-18 15:01:24 +0200

    Added support for Passwords containing only Letters

--- PasswordGenerator/PasswordGeneratorFunctions/PasswordGeneratorFunctions.cs
+++ PasswordGenerator/PasswordGeneratorFunctions/PasswordGeneratorFunctions.cs
@@ -2,16 +2,34 @@
 
 namespace PasswordGeneratorFunctions{
     public static class PasswordGeneratorFunctions{
-        public static string generatePassword(int length=8) {
-            if (length < 1)
+
+        private static int minAll = 33;
+        private static int maxAll = 126;
+
+        private static int minOnlyLetters= 65;
+        private static int maxOnlyLetters = 122;
+
+        public static string generatePassword(int length = 8, bool onlyLetters = false) {
+            if (length < 1) {
                 return null;
+            }
             char[] passArr = new char[length];
             Random rnd = new Random();
+            int min, max;
+            if (onlyLetters) {
+                min = minOnlyLetters;
+                max = maxOnlyLetters;
+            }
+            else {
+                min = minAll;
+                max = maxAll;
+            }
             for (int i = 0; i < length; i++) {
-                passArr[i] = (char)rnd.Next(33, 126);
+                passArr[i] = (char)rnd.Next(min, max);
             }
             return new string(passArr);
         }
+       
 
     }
 }
